ac7361d1328dc75a861037dddb58c8a373b5e479
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2023-10-31 10:42:37 +0800

    feat: setup fonts and routing

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Tasks } from "./screens";
+import { useFonts } from "expo-font";
+import {
+  Inter_100Thin,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter100: Inter_100Thin,
+    Inter: Inter_400Regular,
+    Inter600: Inter_600SemiBold,
+    Inter700: Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (fontError) {
+    console.error("Could not load fonts: ", fontError);
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello Dyna!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator
+        initialRouteName="tasks"
+        screenOptions={{ headerShown: false }}
+      >
+        <Screen name="tasks" component={Tasks} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
